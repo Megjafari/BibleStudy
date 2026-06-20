bbf1968903a30d77c92940585bc544ae502ae75c
Meg Jafari <megberserker@gmail.com>
2026-06-20 15:33:53 +0200

    Add EF Core DbContext, SQL Server configuration and initial migration

--- src/BibleStudy.Api/Program.cs
+++ src/BibleStudy.Api/Program.cs
@@ -1,7 +1,15 @@
+using BibleStudy.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
